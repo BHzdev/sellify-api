79279565cb9f839b831f0644adb554b4d04031ed
BHzdev <bruno_tcc_tc@hotmail.com>
2023-08-29 19:49:03 -0300

    feat: add user_get endpoint

--- endpoints/user_get.php
+++ endpoints/user_get.php
@@ -1,7 +1,26 @@
 <?php
   function api_user_get($request){
     $user = wp_get_current_user();
-    return rest_ensure_response($user);
+    $user_id = $user->ID;
+
+    if($user_id > 0) {
+      $user_meta = get_user_meta($user_id);
+
+      $response = array(
+        "id" => $user->user_login,
+        "nome" => $user->display_name,
+        "email" => $user->user_email,
+        "estado" => $user_meta['estado'][0],
+        "cidade" => $user_meta['cidade'][0],
+        "cep" => $user_meta['cep'][0],
+        "bairro" => $user_meta['bairro'][0],
+        "numero" => $user_meta['numero'][0],
+        "rua" => $user_meta['rua'][0],
+      );
+    }else {
+      $response = new WP_Error('permissao', 'Usuário não possui permissão', array("status" => 401));
+    }
+    return rest_ensure_response($response);
   }
 
   function register_api_user_get() {

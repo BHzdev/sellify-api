5f1891d9dd4559208bd288c62c88a62c4f9b57a7
BHzdev <bruno_tcc_tc@hotmail.com>
2023-08-29 18:49:15 -0300

    fix: functions requires

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 $template_directory = get_template_directory();
 
-require_once($template_directory . "/custom-post-type/produto.php");
-require_once($template_directory . "/custom-post-type/transacao.php");
+require_once($template_directory . "/custom-post-type/product.php");
+require_once($template_directory . "/custom-post-type/transaction.php");
 
 ?>

614654a72061a085f3dfeabbc729af6e773f60c3
BHzdev <bruno_tcc_tc@hotmail.com>
2023-08-30 18:44:13 -0300

    feat: add new route and function in product_get for products get

--- endpoints/product_get.php
+++ endpoints/product_get.php
@@ -49,4 +49,63 @@ function register_api_product_get() {
 }
 add_action('rest_api_init', 'register_api_product_get');
 
+// PRODUCTS
+function api_products_get($request) {
+
+  $q = sanitize_text_field($request['q']) ?: '';
+  $_page = sanitize_text_field($request['_page']) ?: 0;
+  $_limit = sanitize_text_field($request['_limit']) ?: 9;
+  $usuario_id = sanitize_text_field($request['usuario_id']);
+
+  $usuario_id_query = null;
+  if($usuario_id) {
+    $usuario_id_query = array(
+      'key' => 'usuario_id',
+      'value' => $usuario_id,
+      'compare' => '='
+    );
+  }
+
+  $vendido = array(
+    'key' => 'vendido',
+    'value' => 'false',
+    'compare' => '='
+  );
+
+  $query = array(
+    'post_type' => 'produto',
+    'posts_per_page' => $_limit,
+    'paged' => $_page,
+    's' => $q,
+    'meta_query' => array(
+      $usuario_id_query,
+      $vendido,
+    )
+  );
+
+  $loop = new WP_Query($query);
+  $posts = $loop->posts;
+  $total = $loop->found_posts;
+
+  $produtos = array();
+  foreach ($posts as $key => $value) {
+    $produtos[] = product_scheme($value->post_name);
+  }
+
+  $response = rest_ensure_response($produtos);
+  $response->header('X-Total-Count', $total);
+
+  return $response;
+}
+
+function register_api_products_get() {
+  register_rest_route('api', '/produto', array(
+    array(
+      'methods' => WP_REST_Server::READABLE,
+      'callback' => 'api_products_get',
+    ),
+  ));
+}
+add_action('rest_api_init', 'register_api_products_get');
+
 ?>

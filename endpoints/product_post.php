333cf798b60f380fab1c5038e6814fb3acf54dcf
BHzdev <bruno_tcc_tc@hotmail.com>
2023-09-01 20:17:05 -0300

    fix: product_post endpoint method to creatable

--- endpoints/product_post.php
+++ endpoints/product_post.php
@@ -53,7 +53,7 @@
   function register_api_product_post() {
     register_rest_route('api', '/produto', array(
       array(
-        'methods' => WP_REST_Server::EDITABLE,
+        'methods' => WP_REST_Server::CREATABLE,
         'callback' => 'api_product_post',
       ),
     ));
